7ae96ce988271a87a45b82f4ea89b2832231b4ea
Chase Caster <chasec.darkhorse@gmail.com>
2024-02-19 19:55:12 -0600

    mock out data, set up document template

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from "./data.json"
+
+
+function ContactInfo({ contact_info }: {contact_info: Array<string>}) {
+  return (
+    <div id="contact">
+      {contact_info.map((info, i) => <span>{info}</span>)}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="content">
+      <div id="title">
+        <h1>Chase Caster</h1>
+        <h2>Senior Software Engineer</h2>
+        <h4>engineering reliable, resilient systems</h4>
+        <h4>and writing maintainable, test-driven code</h4>
+      </div>
+      <ContactInfo contact_info={data.contact} />
+      <div id="columns">
+        <div className="column"></div>
+        <div className="column"></div>
+      </div>
     </div>
   );
 }
