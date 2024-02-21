ef46378bea83b9fcf72105f45f3d5361b35f596a
Chase Caster <chasec.darkhorse@gmail.com>
2024-02-20 22:41:19 -0600

    hide contact info behind checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 import data from "./data.json"
+import { useState } from "react"
 
 
 function format_url(info: string) {
@@ -16,7 +17,7 @@ function ContactInfo({ contact_info }: {contact_info: Array<string>}) {
     <div id="contact">
       {contact_info.map((info, i) => format_url(info))}
     </div>
-  );
+  )
 }
 
 type Skill = {
@@ -44,7 +45,7 @@ type Education = {
 
 function EducationInfo({education}: {education: Education}) {
   return (
-    <div id="education" className="section">
+    <div id="education" className="section nobreak">
       <div className="title">Education</div>
       <div className="sub title">{education.name}</div>
       <div className="info">
@@ -65,7 +66,7 @@ function PersonalProjects({projects}: {projects: Array<Project>}) {
     <div id="personal" className="section">
       <div className="title">Personal Projects</div>
       {projects.map((project, p) =>
-        <div className="project">
+        <div className="project nobreak">
           <div className="sub title">{project.name}</div>
           <div>{format_url(project.url)}</div>
           <ul>
@@ -90,7 +91,7 @@ function Employment({employers}: {employers: Array<Employer>}) {
     <div id="employment">
       <div className="main title">Professional Experience</div>
       {employers.map((employer, e) =>
-        <div className="employer section">
+        <div className="employer section nobreak">
           <div className="title">
             <span>{employer.name}</span>
             <span>{employer.location}</span>
@@ -116,28 +117,63 @@ function Employment({employers}: {employers: Array<Employer>}) {
   )
 }
 
-function App() {
-  return (
-    <div id="content">
-      <div id="title">
-        <h1>Chase Caster</h1>
-        <h2>Senior Software Engineer</h2>
-        <p id="blurb">
-        {data.blurb.map((line, b) => <span>{line}</span>)}
-        </p>
-      </div>
-      <ContactInfo contact_info={data.contact} />
-      <div id="columns">
-        <div className="column">
-          <Skills skill_groups={data.skills}/>
-          <PersonalProjects projects={data.personal}/>
-          <EducationInfo education={data.education}/>
-        </div>
-        <div className="column"></div>
-          <Employment employers={data.employers}/>
-      </div>
-    </div>
-  );
+type ControlProps = {
+  showContact: boolean, changeShowContact: () => void
 }
 
-export default App;
+function ControlBar({showContact, changeShowContact}: ControlProps) {
+  return (
+    <div id="controls">
+      <label id="show-contact">
+      Contact Info
+      <input type="checkbox" checked={showContact} onChange={changeShowContact}/>
+      </label>
+    </div>
+  )
+}
+
+class App extends React.Component {
+  state: {showContact: boolean}
+
+  constructor(props: {}) {
+    super(props)
+    this.state = {
+      showContact: false
+    }
+    this.changeShowContact = this.changeShowContact.bind(this)
+  }
+
+  changeShowContact() {
+    this.setState({showContact: !this.state.showContact})
+  }
+
+  render() {
+      return (<>
+        <div id="content">
+          <div id="title">
+            <h1>Chase Caster</h1>
+            <h2>Senior Software Engineer</h2>
+            <p id="blurb">
+            {data.blurb.map((line, b) => <span>{line}</span>)}
+            </p>
+          </div>
+          {this.state.showContact ?
+            <ContactInfo contact_info={data.contact} /> : ""
+          }
+          <div id="columns">
+            <div className="column">
+              <Skills skill_groups={data.skills}/>
+              <PersonalProjects projects={data.personal}/>
+              <EducationInfo education={data.education}/>
+            </div>
+            <div className="column"></div>
+              <Employment employers={data.employers}/>
+          </div>
+        </div>
+        <ControlBar showContact={this.state.showContact}
+              changeShowContact={this.changeShowContact}/>
+      </>)
+  }
+}
+
+export default App
