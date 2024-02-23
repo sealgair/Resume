c295a1fd86f83ed7608cb361b1646138665f6ce2
Chase Caster <sealgair@gmail.com>
2024-02-23 15:31:12 -0600

    handle small screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 import Selectable from './Selectable'
 import ControlBar from './ControlBar'
--- src/ControlBar.tsx
+++ src/ControlBar.tsx
@@ -23,24 +23,26 @@ function ControlBar({showContact, changeShowContact, summarize, changeSummarize}
 
   return (
     <div id="controls">
-      <label id="summarize">
-        Summarize Past Employment
-        <input type="checkbox" checked={summarize} onChange={changeSummarize}/>
-      </label>
-      <button id="hide-selected" disabled={!canHide} onClick={() => {
-        document.dispatchEvent(new Event("control:hide"))
-      }}>
-        Hide Selected
-      </button>
-      <button id="unhide-all" disabled={!canUnide} onClick={() => {
-        document.dispatchEvent(new Event("control:unhide"))
-      }}>
-        Unhide All
-      </button>
-      <label id="show-contact">
-        Show Contact Info
-        <input type="checkbox" checked={showContact} onChange={changeShowContact}/>
-      </label>
+        <label id="show-contact">
+            Show Contact Info
+            <input type="checkbox" checked={showContact} onChange={changeShowContact}/>
+        </label>
+        <label id="summarize">
+            Summarize Past Employment
+            <input type="checkbox" checked={summarize} onChange={changeSummarize}/>
+        </label>
+        <span id="hide-buttons">
+            <button id="hide-selected" disabled={!canHide} onClick={() => {
+                    document.dispatchEvent(new Event("control:hide"))
+            }}>
+                Hide Selected
+            </button>
+            <button id="unhide-all" disabled={!canUnide} onClick={() => {
+                    document.dispatchEvent(new Event("control:unhide"))
+            }}>
+                Unhide All
+            </button>
+        </span>
     </div>
   )
 }
